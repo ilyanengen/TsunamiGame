4aeaff22f2e995e0063d975fc1745b5a11fa5448
Ilya Biltuev <ilya@ilya-pc.local>
2017-01-21 12:49:24 +0300

    added methods for player and wave

--- TsunamiGame/GameScene.m
+++ TsunamiGame/GameScene.m
@@ -13,6 +13,11 @@
     
     CGFloat screenHeight;
     CGFloat screenWidth;
+    CGSize screenCell;
+    
+    SKSpriteNode *_player;
+    SKSpriteNode *_wave;
+    SKSpriteNode *_background;
 }
 
 - (void)didMoveToView:(SKView *)view {
@@ -20,12 +25,46 @@
     //Get screen size to use later
     screenWidth = view.bounds.size.width;
     screenHeight = view.bounds.size.height;
+    NSLog(@"\n\nscreenWidth = %f \nscreenHeight = %f\n\n", screenWidth, screenHeight);
     
+    screenCell = CGSizeMake(screenWidth/5, screenWidth/5);
+    NSLog(@"screenCell = (%f, %f)", screenCell.width, screenCell.height);
+    
+    //add player
+    [self addPlayer];
+    
+    //add wave
+    [self addWave];
 }
 
 
 -(void)update:(CFTimeInterval)currentTime {
     // Called before each frame is rendered
+}
+
+#pragma mark - Add main nodes
+- (void)addPlayer {
+
+    CGSize playerSize = CGSizeMake(screenCell.width, screenCell.width * 2);
+    SKSpriteNode *player = [SKSpriteNode spriteNodeWithColor:[SKColor redColor] size:playerSize];
+    player.anchorPoint = CGPointMake(0.5, 0.5);
+    player.zPosition = 10;
+    
+    player.position = CGPointMake(screenWidth/2, screenHeight/2 - screenCell.height);
+    _player = player;
+    [self addChild:_player];
+}
+
+- (void)addWave {
+
+    CGSize waveSize = CGSizeMake(screenWidth, screenCell.width);
+    SKSpriteNode *wave = [SKSpriteNode spriteNodeWithColor:[SKColor blueColor] size:waveSize];
+    wave.anchorPoint = CGPointMake(0,0);
+    wave.zPosition = 11;
+    
+    wave.position = CGPointZero;
+    _wave = wave;
+    [self addChild:_wave];
 }
 
 #pragma mark - TOUCHES
