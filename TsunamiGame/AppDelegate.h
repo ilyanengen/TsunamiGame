a9581971e0038b6a38df91b144b326bb4ac84e19
Ilya Biltuev <ilya@ilya-pc.local>
2017-01-21 10:52:46 +0300

    Initial Commit

---
+++ TsunamiGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TsunamiGame
+//
+//  Created by Илья on 21.01.17.
+//  Copyright © 2017 Ilya Biltuev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TsunamiGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  TsunamiGame
+//
+//  Created by Илья on 21.01.17.
+//  Copyright © 2017 Ilya Biltuev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
